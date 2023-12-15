8143aaed9fa01fb4caffbf599ecc2e38795a91e3
Souhei Kodama <1419033@ed.tus.ac.jp>
2023-12-15 19:54:06 +0900

    fix

--- main.py
+++ main.py
@@ -41,7 +41,9 @@ def main():
     parser.add_argument("--n_head", type=int, default=8)
     parser.add_argument("--num_blocks", type=int, default=2)
     parser.add_argument("--temporal_mode", type=str, default="BRPE", help='["BRPE", "None"]')
-    parser.add_argument("--spatial_mode", type=str, default="AFT-simple", help='["AFT-full", "AFT-simple", "None"]')
+    parser.add_argument(
+        "--spatial_mode", type=str, default="AFT-simple", help='["AFT-KVR", "AFT-full", "AFT-simple", "None"]'
+    )
     parser.add_argument("--use_only", type=str, default=None, help='["temporal", "spatial", "None"]')
     parser.add_argument("--connection", type=str, default="parallel", help='["parallel", "series_t", "series_s"]')
     parser.add_argument("--save_attention", type=bool, default=False, help="save attention")
