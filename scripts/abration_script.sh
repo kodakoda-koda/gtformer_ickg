efea3ffdad2bafd82a7a9eac9779d9a83a3dc018
Sohei Kodama <soheikodama@SoheinoMacBook-Pro.local>
2023-12-19 20:38:26 +0900

    fix

--- scripts/abration_script.sh
+++ scripts/abration_script.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
 # base
-python main.py --data_type Taxi
-
-# use only
-python main.py --data_type Taxi --use_only temporal
-python main.py --data_type Taxi --use_only spatial
-
-# spatial mode
-python main.py --data_type Taxi --spatial_mode AFT-full --dtype bf16
-python main.py --data_type Taxi --spatial_mode None --dtype bf16
-
-# temporal mode
-python main.py --data_type Taxi --temporal_mode None
-
-# base
 python main.py
 
 # use only
@@ -22,8 +8,8 @@ python main.py --use_only temporal
 python main.py --use_only spatial
 
 # spatial mode
-python main.py --spatial_mode AFT-full --dtype bf16
-python main.py --spatial_mode None --dtype bf16
+python main.py --spatial_mode AFT-full # --dtype bf16
+python main.py --spatial_mode None # --dtype bf16
 
 # temporal mode
 python main.py --temporal_mode None
