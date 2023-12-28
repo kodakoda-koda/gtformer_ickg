dd735c3c1c14cc6bc30a3bd7ff24972f85b9ddd4
Souhei Kodama <1419033@ed.tus.ac.jp>
2023-12-28 17:25:54 +0900

    fix

--- scripts/abration_script.sh
+++ scripts/abration_script.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
 # base
-python main.py
-
-# use only
-python main.py --use_only temporal
-python main.py --use_only spatial
-
-# spatial mode
-# python main.py --spatial_mode AFT-full
-# python main.py --spatial_mode None
-
-# temporal mode
-python main.py --temporal_mode None
-
-# base
 python main.py --data_type Taxi
 
 # use only
@@ -22,8 +8,9 @@ python main.py --data_type Taxi --use_only temporal
 python main.py --data_type Taxi --use_only spatial
 
 # spatial mode
-# python main.py --data_type Taxi --spatial_mode AFT-full
-# python main.py --data_type Taxi --spatial_mode None
+python main.py --data_type Taxi --spatial_mode None
 
 # temporal mode
 python main.py --data_type Taxi --temporal_mode None
+
+python main.py --data_type Taxi --spatial_mode None --tamporal_mode None
