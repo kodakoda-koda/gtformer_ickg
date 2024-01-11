4f20533cf4353680ce09cc0766b4f6121107b226
Souhei Kodama <1419033@ed.tus.ac.jp>
2024-01-11 16:59:50 +0900

    fix

--- main.py
+++ main.py
@@ -59,8 +59,8 @@ def main():
             args.num_tiles = 99  # 54
             args.tile_size = "5000m"  # 7500m
     else:
-        args.num_tiles = 144  # 144
-        args.tile_size = "1000m"  # 1000m
+        args.num_tiles = 144  # 68
+        args.tile_size = "1000m"  # 2000m
 
     if args.dtype == "bf16":
         args.dtype = torch.bfloat16
