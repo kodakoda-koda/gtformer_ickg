9849f267a674f306b8e054620f7b7f5e5d56d4be
Souhei Kodama <gundam634rx78.2@gmail.com>
2024-05-14 13:14:10 +0900

    fix

--- layers/bgarn_layers.py
+++ layers/bgarn_layers.py
@@ -9,14 +9,15 @@ class AttentionNet(nn.Module):
     def __init__(self, d_model):
         super(AttentionNet, self).__init__()
         self.linear1 = nn.Linear(d_model, d_model)
-        self.linear2 = nn.Linear(d_model, 1)
-        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(d_model, d_model)
+        self.linear3 = nn.Linear(d_model, 1)
+        self.tanh = nn.Tanh()
 
     def forward(self, X, Y):
         X_ = self.linear1(X.view(-1, X.shape[-2], X.shape[-1])).permute(0, 2, 1).unsqueeze(-1)
-        Y_ = self.linear1(Y.view(-1, X.shape[-2], X.shape[-1])).permute(0, 2, 1).unsqueeze(-2)
-        net = torch.matmul(X_, Y_).permute(0, 2, 3, 1)
-        out = self.relu(self.linear2(net)).squeeze()
+        Y_ = self.linear2(Y.view(-1, X.shape[-2], X.shape[-1])).permute(0, 2, 1).unsqueeze(-2)
+        net = self.tanh(torch.matmul(X_, Y_).permute(0, 2, 3, 1))
+        out = self.linear3(net).squeeze()
 
         return out.view(X.shape[0], -1, X.shape[-2], X.shape[-2])
 
@@ -24,33 +25,44 @@ class AttentionNet(nn.Module):
 class SpatialAttentionLayer(nn.Module):
     def __init__(self, d_model):
         super(SpatialAttentionLayer, self).__init__()
-        self.attention_net = AttentionNet(d_model)
-        self.linear = nn.Linear(d_model, d_model)
+        self.attention_net_A = AttentionNet(d_model)
+        self.attention_net_B = AttentionNet(d_model)
+        self.attention_net_C = AttentionNet(d_model)
+        self.linear1 = nn.Linear(d_model, d_model)
+        self.linear2 = nn.Linear(d_model, d_model)
+        self.linear3 = nn.Linear(d_model, d_model)
+        self.linear4 = nn.Linear(d_model, d_model)
         self.softmax = nn.Softmax(dim=-2)
+        self.relu = nn.ReLU()
+        self.d_model = d_model
 
     def forward(self, X):
-        B, L, O, D = X.shape
+        B_, L, O, D = X.shape
 
-        A = (X + 1e-5) / (X.sum(dim=-1, keepdim=True) + 1e-5)  # B, L, O, D
-        B = (X + 1e-5) / (X.sum(dim=-2, keepdim=True) + 1e-5)
-        C = torch.reciprocal(X + 1e-5) / (torch.sum(torch.reciprocal(X + 1e-5), dim=(-1), keepdim=True) + 1e-5)
+        e = 1e-10
+        A = (X) / (X.sum(dim=-1, keepdim=True) + e)  # B, L, O, D
+        B = (X) / (X.sum(dim=-2, keepdim=True) + e)
+        C = torch.reciprocal(X + e) / (torch.sum(torch.reciprocal(X + e), dim=(-1), keepdim=True))
 
-        V = torch.ones(B, L, O, self.d_model).to(X.device)  # B, L, O, d_model
+        V = torch.ones((B_, L, O, self.d_model)).to(X.device)  # B, L, O, d_model
 
         A_V = torch.matmul(A, V)  # B, L, O, d_model
         B_V = torch.matmul(B, V)
         C_V = torch.matmul(C, V)
 
-        A_net = self.attention_net(V, A_V)  # B, L, O, D
-        B_net = self.attention_net(V, B_V)
-        C_net = self.attention_net(V, C_V)
+        A_net = self.attention_net_A(V, A_V)  # B, L, O, D
+        B_net = self.attention_net_B(V, B_V)
+        C_net = self.attention_net_C(V, C_V)
 
         phi = self.softmax(A_net)  # B, L, O, D
         psi = self.softmax(B_net)
         theta = self.softmax(C_net)
 
-        V_ = self.linear(V)  # B, L, O, d_model
-
-        M = V_ + torch.matmul(phi, V_) + torch.matmul(psi, V_) + torch.matmul(theta, V_)  # B, L, O, d_model
+        M = (
+            self.relu(self.linear1(V))
+            + torch.matmul(phi, self.relu(self.linear2(V)))
+            + torch.matmul(psi, self.relu(self.linear3(V)))
+            + torch.matmul(theta, self.relu(self.linear4(V)))
+        )  # B, L, O, d_model
 
         return M
--- model/BGARN.py
+++ model/BGARN.py
@@ -11,7 +11,9 @@ class Model(nn.Module):
         self.attention_net = AttentionNet(args.d_model)
         self.spatial_layer = SpatialAttentionLayer(args.d_model)
         self.temporal_layer = nn.LSTM(input_size=args.d_model, hidden_size=args.d_model, num_layers=1, batch_first=True)
+        self.linear = nn.Linear(args.d_model, args.d_model)
         self.bn = nn.BatchNorm1d(args.d_model)
+        self.relu = nn.ReLU()
         self.d_model = args.d_model
 
     def forward(self, X, _):
@@ -25,8 +27,8 @@ class Model(nn.Module):
         M_T = self.temporal_layer(M.view(-1, L, self.d_model))[0][:, -1, :].view(B, O, self.d_model)  # B, O, d_model
         M_T = self.bn(M_T.permute(0, 2, 1)).permute(0, 2, 1)  # B, O, d_model
 
-        M_T = self.linear(M_T)  # B, O, d_model
+        M_T = self.relu(self.linear(M_T))  # B, O, d_model
         G = self.attention_net(M_T, M_T)  # B, 1, O, D
-        out = G * X.mean(dim=1, keepdim=True)  # B, 1, O, D
+        out = G + X.mean(dim=1, keepdim=True)  # B, 1, O, D
 
         return out
